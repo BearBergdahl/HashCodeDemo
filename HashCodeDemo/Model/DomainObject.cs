c3408a0f73bc5e9461fd333a71f549b0eae7d349
Björn Bergdahl <84767875+BearBergdahl@users.noreply.github.com>
2023-11-21 10:54:53 +0100

    Add project files.

---
+++ HashCodeDemo/Model/DomainObject.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace HashCodeDemo.Model
+{
+    internal class DomainObject
+    {
+        //public Guid Id { get; set; }
+        public string CityCode { get; set; }
+        public int LotId { get; set; } 
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public int CurrentBatteryLevel { get; set; }
+
+        public DomainObject() { }
+        public DomainObject(string cityCode, int lotId, string name, string description, string type, int currentBatteryLevel) 
+        { 
+            this.CityCode = cityCode;
+            this.LotId = lotId;
+            this.Name = name;
+            this.Description = description; 
+            this.Type = type;   
+            this.CurrentBatteryLevel = currentBatteryLevel;            
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is DomainObject @object &&
+                   CityCode == @object.CityCode &&
+                   LotId == @object.LotId &&
+                   Name == @object.Name;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(CityCode, LotId, Name);
+        }
+
+
+        //public override bool Equals(object? obj)
+        //{
+        //    DomainObject obj2 = obj as DomainObject;
+        //    if (obj2 == null) return false;
+        //    if (!this.Id.Equals(obj2.Id)) return false;
+        //    return true;
+        //}
+
+        //public override int GetHashCode()
+        //{
+        //    unchecked
+        //    {
+        //        int hash = 17;
+        //        hash = hash * 23 + Id.GetHashCode();
+        //        return hash;
+        //    }            
+        //}
+    }
+}
---
+++ HashCodeDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using HashCodeDemo.Model;
+using System.Security.Cryptography.X509Certificates;
+
+namespace HashCodeDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DomainObject myElectricBike = new DomainObject("Link013",147,"GreenRenter19", "Electric bike 12Wh", "Electrobike", 24);
+            DomainObject alsoMyElectricBike = new DomainObject
+            {
+                LotId = myElectricBike.LotId,
+                CityCode = myElectricBike.CityCode,
+                Name = myElectricBike.Name,
+                Description = myElectricBike.Description,
+                Type = myElectricBike.Type,
+                CurrentBatteryLevel = 4            
+            };
+            if (myElectricBike.Equals(alsoMyElectricBike))
+            {
+                HashSet<DomainObject> availableBikes = new HashSet<DomainObject>();
+                availableBikes.Add(myElectricBike);
+                availableBikes.Add(alsoMyElectricBike);
+            }
+            if(myElectricBike.Equals(alsoMyElectricBike))
+            {
+                Console.WriteLine("Success!");
+                if (myElectricBike.GetHashCode() == alsoMyElectricBike.GetHashCode())
+                {
+                    Console.WriteLine("Yay! We made it!");
+                }
+                else { Console.WriteLine("Not yet!"); }
+            }
+            Console.WriteLine(myElectricBike.GetHashCode());
+            Console.WriteLine(alsoMyElectricBike.GetHashCode());           
+
+        }
+    }
+}
